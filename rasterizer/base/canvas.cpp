1a0b70aea8bcf7054b73f8c2c12631c2a22e01d3
AmbBAI <amb.main@gmail.com>
2015-04-16 19:05:13 +0800

    to inline

--- rasterizer/base/canvas.cpp
+++ rasterizer/base/canvas.cpp
@@ -16,65 +16,6 @@ Canvas::Canvas(int width, int height)
 	}
 }
 
-bool Canvas::SetPixel(int x, int y, const Color& color)
-{
-	if (x < 0 || x >= width) return false;
-	if (y < 0 || y >= height) return false;
-
-	SetPixel(x, y, Color32(color));
-	return true;
-}
-
-bool Canvas::SetPixel(int x, int y, float depth, const Color& color)
-{
-	if (x < 0 || x >= width) return false;
-	if (y < 0 || y >= height) return false;
-
-	int offset = y * width + x;
-	if (depths[offset] < depth) return true;
-	depths[offset] = depth;
-	SetPixel(x, y, Color32(color));
-	return true;
-}
-
-bool Canvas::SetPixel(int x, int y, const Color32& color)
-{
-	if (x < 0 || x >= width) return false;
-	if (y < 0 || y >= height) return false;
-
-	int offset = y * width + x;
-	pixels[offset] = color.rgba;
-	return true;
-}
-
-float Canvas::GetDepth(int x, int y)
-{
-	if (x < 0 || x >= width) return 1.f;
-	if (y < 0 || y >= height) return 1.f;
-
-	int offset = y * width + x;
-	return depths[offset];
-}
-
-void Canvas::SetDepth(int x, int y, float depth)
-{
-	if (x < 0 || x >= width) return;
-	if (y < 0 || y >= height) return;
-
-	int offset = y * width + x;
-	depths[offset] = depth;
-}
-
-int Canvas::GetWidth()
-{
-	return width;
-}
-
-int Canvas::GetHeight()
-{
-	return height;
-}
-
 void Canvas::Clear()
 {
 	pixels.assign(width * height, 0);
