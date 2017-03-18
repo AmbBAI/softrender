b8b2b80e09ca923201051feb8db54721e26e83a3
AmbBAI <amb.main@gmail.com>
2017-03-18 19:20:06 +0800

    unity's choice

--- softrender/softrender/pbsf.hpp
+++ softrender/softrender/pbsf.hpp
@@ -28,19 +28,12 @@ struct PBSF
 		return a2 / (Mathf::PI * d * d);
 	}
 
-	static float VisibilityTerm(float nDotL, float nDotV, float roughness)
+	static float SmithJointGGXVisibilityTerm(float nDotL, float nDotV, float roughness)
 	{
-		float k = (roughness + 1.f) * (roughness + 1.f) / 8.f;
-		float gl = nDotL / (nDotL * (1.f - k) + k);
-		float gv = nDotV / (nDotV * (1.f - k) + k);
-		return gl * gv;
-	}
-
-	static float SmithVisibilityTerm(float nDotL, float nDotV, float k)
-	{
-		float gl = nDotL * (1 - k) + k;
-		float gv = nDotV * (1 - k) + k;
-		return 1.f / (gl * gv + 1e-5f);
+		float a = roughness * roughness;
+		float lambdaV = nDotL * (nDotV * (1.f - a) + a);
+		float lambdaL = nDotV * (nDotL * (1.f - a) + a);
+		return 0.5f / (lambdaV + lambdaL + 1e-5f);
 	}
 
 	static float FresnelTerm(float vDotH, float specular)
@@ -53,28 +46,27 @@ struct PBSF
 		float nlPow5 = Mathf::Pow(1.f - nDotL, 5.f);
 		float nvPow5 = Mathf::Pow(1.f - nDotV, 5.f);
 		float fD90 = 0.5f + 2.f * lDotH * lDotH * roughness;
-		return (1.f + (fD90 - 1.f) * nlPow5) * (1.f + (fD90 - 1.f) * nvPow5) / Mathf::PI;
+		return Mathf::Clamp01((1.f + (fD90 - 1.f) * nlPow5) * (1.f + (fD90 - 1.f) * nvPow5));
 	}
 
 	static ColorRGB BRDF(const PBSInput& input, const Vector3& lightDir, const Vector3& viewDir)
 	{
-		Vector3 floatDir = (lightDir + viewDir).Normalize();
-		float nDotL = input.normal.Dot(lightDir);
-		float nDotV = input.normal.Dot(viewDir);
-		float nDotH = input.normal.Dot(floatDir);
-		float vDotH = viewDir.Dot(floatDir);
-		float lDotH = lightDir.Dot(floatDir);
+		Vector3 halfdir = (lightDir + viewDir).Normalize();
+		float nDotL = Mathf::Clamp01(input.normal.Dot(lightDir));
+		float nDotV = Mathf::Clamp01(input.normal.Dot(viewDir));
+		float nDotH = Mathf::Clamp01(input.normal.Dot(halfdir));
+		float vDotH = Mathf::Clamp01(viewDir.Dot(halfdir));
+		float lDotH = Mathf::Clamp01(lightDir.Dot(halfdir));
 
 		float diffuseTerm = DisneyDiffuseTerm(nDotL, nDotV, lDotH, input.roughness);
 		float D = GGXTerm(nDotH, input.roughness);
-		float V = SmithVisibilityTerm(nDotL, nDotV, input.roughness);
+		float V = SmithJointGGXVisibilityTerm(nDotL, nDotV, input.roughness);
 		float F = FresnelTerm(vDotH, input.specular);
-		//return input.albedo * diffuseTerm + Color::white.rgb * (D * V * F / (4.f * nDotL * nDotV));
-		return input.albedo * diffuseTerm;
+		return input.albedo * (diffuseTerm * nDotL) + Color::white.rgb * (D * V * F * nDotL * Mathf::PI);
+		//return Color::white.rgb * (diffuseTerm * nDotL);
 		//return Color::white.rgb * D;
 		//return Color::white.rgb * V;
 		//return Color::white.rgb * F;
-		//return Color::white.rgb * (1.f / (4.f * nDotL * nDotV));
 	}
 
 	static ColorRGB BRDF2(const PBSInput& input, const Vector3& lightDir, const Vector3& viewDir)
